5d846cac03c0a9cfadfec7d7a85662d2a0ce0489
Komal Dindiyal <komald.4@gmail.com>
2020-02-04 18:41:02 +0000

    Automatically backed up by Learn

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,12 @@
 class Lifter
   attr_reader :name, :lift_total
-
+  @@all = []
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+  end
+
+  def lifters
+    
   end
 end
