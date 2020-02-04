4c6ee341bf3fe6248c263a0f1c94f20ae35b556c
Komal Dindiyal <komald.4@gmail.com>
2020-02-04 18:30:54 +0000

    Automatically backed up by Learn

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,9 +1,12 @@
 class Membership
   attr_reader :cost
-
+  @@all = []
   def initialize(cost, gym, lifter)
     @cost = cost
     @gym = gym
     @lifter = lifter
+
+    @@all << self
+    
   end
 end
